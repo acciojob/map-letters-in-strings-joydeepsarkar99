9e72bfbe6436029373ddcca6f36230e382e6fd66
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-18 14:37:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetters(str) {
+		let obj = {}
+	for(let i=0;i<str.length;i++){
+	  let s = str[i]
+	  if(s in obj == false){
+	    obj[s] = []
+	  }
+	  obj[s].push(i)
+	}
+	return obj
+}
+
+
+
+
+const s = prompt("Enter String:");
+alert(mapLetters(s));
